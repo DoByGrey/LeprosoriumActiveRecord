076fa3834f5c949a2e6d26b6bc9d76cb9ddc016d
Savinkin Sergey <ssavinkin13@yandex.ru>
2022-06-22 22:40:48 +0300

    Add create_posts migration

---
+++ db/migrate/20220622193637_Create_Posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+
+    create_table :posts do |t|
+      t.text :created_date
+      t.text :author
+      t.text :content
+
+      t.timestamps
+    end  
+
+  end
+end
